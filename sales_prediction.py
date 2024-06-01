b9e77d2c02b0521a10cd8e98c24751f5b4f56e17
SHREYA GUPTA <110915606+SHREYA-2003@users.noreply.github.com>
2024-06-01 13:07:41 +0530

    Create sales_prediction.py

---
+++ sales_prediction.py
@@ -0,0 +1,30 @@
+# Importing necessary libraries
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load the sales data
+sales_data = pd.read_csv("/content/Sales.csv")  # Assuming you have a CSV file with sales data
+
+# Extracting features and target variable
+X = sales_data[['TV', 'Radio','Newspaper' ]]
+y = sales_data['Sales']
+
+# Splitting the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Creating and training the linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Making predictions on the testing set
+y_pred = model.predict(X_test)
+
+# Evaluating the model
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Mean Squared Error:", mse)
+print("R^2 Score:", r2)
